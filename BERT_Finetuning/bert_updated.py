3473533504e9f034e4b6cafa02156afaddb95c5b
Procheta Sen <prochetasen@Prochetas-MacBook-Pro.local>
2025-03-02 22:59:16 +0000

    Updated

---
+++ BERT_Finetuning/bert_updated.py
@@ -0,0 +1,312 @@
+import pandas as pd
+from dGClip_bert import *
+import sys
+
+df = pd.read_csv('train_sub.csv')
+
+from transformers import DistilBertTokenizer
+
+tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased', do_lower_case=True)
+
+texts = df['question_text'].values
+labels = df['target'].values
+text_ids = [tokenizer.encode(text, max_length=300, pad_to_max_length=True) for text in texts]
+
+
+att_masks = []
+for ids in text_ids:
+    masks = [int(id > 0) for id in ids]
+    att_masks.append(masks)
+
+
+from sklearn.model_selection import train_test_split
+
+train_x, test_val_x, train_y, test_val_y = train_test_split(text_ids, labels, random_state=111, test_size=0.2)
+train_m, test_val_m = train_test_split(att_masks, random_state=111, test_size=0.2)
+
+test_x, val_x, test_y, val_y = train_test_split(test_val_x, test_val_y, random_state=111, test_size=0.5)
+test_m, val_m = train_test_split(test_val_m, random_state=111, test_size=0.5)
+
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+
+batch_size = 32
+
+import torch
+
+train_x = torch.tensor(train_x)
+test_x = torch.tensor(test_x)
+val_x = torch.tensor(val_x)
+train_y = torch.tensor(train_y)
+test_y = torch.tensor(test_y)
+val_y = torch.tensor(val_y)
+train_m = torch.tensor(train_m)
+test_m = torch.tensor(test_m)
+val_m = torch.tensor(val_m)
+
+
+
+
+
+
+train_data = TensorDataset(train_x, train_m, train_y)
+train_sampler = RandomSampler(train_data)
+train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
+
+val_data = TensorDataset(val_x, val_m, val_y)
+val_sampler = SequentialSampler(val_data)
+val_dataloader = DataLoader(val_data, sampler=val_sampler, batch_size=batch_size)
+
+from transformers import DistilBertForSequenceClassification, AdamW, DistilBertConfig
+
+num_labels = len(set(labels))
+
+model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased', num_labels=num_labels,output_attentions=False, output_hidden_states=False)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+model = model.to(device)
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+print('Number of trainable parameters:', count_parameters(model), '\n', model)
+
+
+learning_rate = 1e-5
+adam_epsilon = 1e-8
+
+no_decay = ['bias', 'LayerNorm.weight']
+#optimizer_grouped_parameters = [
+ #   {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+  #   'weight_decay_rate': 0.2},
+   # {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
+    # 'weight_decay_rate': 0.0}
+#]
+
+#optimizer = AdamW(optimizer_grouped_parameters, lr=learning_rate, eps=adam_epsilon)
+optimizer=dGClip(model.parameters(), lr=float(sys.argv[1]), delta=float(sys.argv[2]))
+from transformers import get_linear_schedule_with_warmup
+
+num_epochs = 15
+total_steps = len(train_dataloader) * num_epochs
+
+
+
+import time
+
+def epoch_time(start_time, end_time):
+    elapsed_time = end_time - start_time
+    elapsed_mins = int(elapsed_time / 60)
+    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+    return elapsed_mins, elapsed_secs
+
+
+
+import numpy as np
+import random
+
+seed_val = 111
+
+random.seed(seed_val)
+np.random.seed(seed_val)
+torch.manual_seed(seed_val)
+torch.cuda.manual_seed_all(seed_val)
+
+
+
+train_losses = []
+val_losses = []
+num_mb_train = len(train_dataloader)
+num_mb_val = len(val_dataloader)
+
+if num_mb_val == 0:
+    num_mb_val = 1
+
+test_data = TensorDataset(test_x, test_m)
+test_sampler = SequentialSampler(test_data)
+test_dataloader = DataLoader(test_data, sampler=test_sampler, batch_size=batch_size)
+
+
+
+acc=[]
+for n in range(num_epochs):
+    train_loss = 0
+    val_loss = 0
+    start_time = time.time()
+
+    for k, (mb_x, mb_m, mb_y) in enumerate(train_dataloader):
+        optimizer.zero_grad()
+        model.train()
+
+        mb_x = mb_x.to(device)
+        mb_m = mb_m.to(device)
+        mb_y = mb_y.to(device)
+
+        outputs = model(mb_x, attention_mask=mb_m, labels=mb_y)
+
+        loss = outputs[0]
+        #loss = model_loss(outputs[1], mb_y)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.data / num_mb_train
+        #train_losses.append(loss.data.cpu())
+    val_loss=0
+    with torch.no_grad():
+            model.eval()
+
+            for k, (mb_x, mb_m, mb_y) in enumerate(val_dataloader):
+                    mb_x = mb_x.to(device)
+                    mb_m = mb_m.to(device)
+                    mb_y = mb_y.to(device)
+
+                    outputs = model(mb_x, attention_mask=mb_m, labels=mb_y)
+
+                    loss = outputs[0]
+                    #loss = model_loss(outputs[1], mb_y)
+                    val_loss += loss.data / num_mb_val
+
+    print ("Validation loss after itaration %i: %f" % (n+1, val_loss))
+    val_losses.append(val_loss.cpu())
+
+    print ("\nTrain loss after itaration %i: %f" % (n+1, train_loss))
+    train_losses.append(train_loss.cpu())
+
+    outputs_new = []
+    with torch.no_grad():
+        model.eval()
+        for k, (mb_x, mb_m) in enumerate(test_dataloader):
+                mb_x = mb_x.to(device)
+                mb_m = mb_m.to(device)
+
+                output = model(mb_x, attention_mask=mb_m)
+                outputs_new.append(output[0].to('cpu'))
+        outputs = torch.cat(outputs_new)
+
+
+        _, predicted_values = torch.max(outputs, 1)
+        predicted_values = predicted_values.numpy()
+        true_values = test_y.numpy()
+        test_accuracy = np.sum(predicted_values == true_values) / len(true_values)
+        print ("Test Accuracy:", test_accuracy)
+        acc.append(test_accuracy)
+    with torch.no_grad():
+        model.eval()
+
+        for k, (mb_x, mb_m, mb_y) in enumerate(val_dataloader):
+            mb_x = mb_x.to(device)
+            mb_m = mb_m.to(device)
+            mb_y = mb_y.to(device)
+
+            outputs = model(mb_x, attention_mask=mb_m, labels=mb_y)
+
+            loss = outputs[0]
+            #loss = model_loss(outputs[1], mb_y)
+            #val_loss += loss.data / num_mb_val
+
+        #print ("Validation loss after itaration %i: %f" % (n+1, val_loss))
+        #val_losses.append(val_loss.cpu())
+
+    end_time = time.time()
+    epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+    print(f'Time: {epoch_mins}m {epoch_secs}s')
+
+
+import pickle
+import os
+
+out_dir = './model'
+
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+
+model_to_save = model.module if hasattr(model, 'module') else model
+model_to_save.save_pretrained(out_dir)
+tokenizer.save_pretrained(out_dir)
+
+with open(out_dir + '/train_losses.pkl', 'wb') as f:
+    pickle.dump(train_losses, f)
+
+with open(out_dir + '/val_losses.pkl', 'wb') as f:
+    pickle.dump(val_losses, f)
+
+
+out_dir = './model'
+
+model = DistilBertForSequenceClassification.from_pretrained(out_dir)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = model.to(device)
+
+with open(out_dir + '/train_losses.pkl', 'rb') as f:
+    train_losses = pickle.load(f)
+    
+with open(out_dir + '/val_losses.pkl', 'rb') as f:
+    val_losses = pickle.load(f)
+
+from matplotlib import pyplot as plt
+
+epochs=[]
+for i in range(0,len(train_losses)):
+    epochs.append(i)
+
+
+ticks=[]
+
+for i in range(0,num_epochs):
+    ticks.append(i)
+
+plt.figure()
+plt.plot(train_losses,label="Train Loss")
+plt.plot(val_losses, label="Validation Loss")
+plt.xlabel("Number of Epochs")
+plt.ylabel("Loss Value")
+plt.xticks(ticks, ticks,rotation="vertical")
+plt.xlim(0,len(train_losses)-1)
+plt.legend()
+plt.savefig("bert_loss.png")
+
+
+with open("loss_values.txt","w") as f:
+	for i in range(num_epochs):
+		f.write(str(i))
+		f.write("\t")
+		f.write(str(train_losses[i]))
+		f.write("\t")
+		f.write(str(val_losses[i]))
+		f.write("\n")
+f.close()
+
+with open("acc_values.txt","w") as f:
+        for i in range(num_epochs):
+                f.write(str(i))
+                f.write("\t")
+                f.write(str(acc[i]))
+                f.write("\n")
+f.close()
+
+batch_size = 32
+
+test_data = TensorDataset(test_x, test_m)
+test_sampler = SequentialSampler(test_data)
+test_dataloader = DataLoader(test_data, sampler=test_sampler, batch_size=batch_size)
+
+outputs = []
+with torch.no_grad():
+    model.eval()
+    for k, (mb_x, mb_m) in enumerate(test_dataloader):
+        mb_x = mb_x.to(device)
+        mb_m = mb_m.to(device)
+        output = model(mb_x, attention_mask=mb_m)
+        outputs.append(output[0].to('cpu'))
+
+outputs = torch.cat(outputs)
+
+
+_, predicted_values = torch.max(outputs, 1)
+predicted_values = predicted_values.numpy()
+true_values = test_y.numpy()
+test_accuracy = np.sum(predicted_values == true_values) / len(true_values)
+print ("Test Accuracy:", test_accuracy)
+
+print("end")
